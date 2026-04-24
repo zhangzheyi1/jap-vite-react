a02981c52a6982c30cd940f4afb0e4a4f859f386
zhangzhy1 <zhangzhy1@yonyou.com>
2026-04-24 14:35:29 +0800

    add gitpages config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
+  plugins: [tailwindcss(), reactRouter(), react()],
   resolve: {
     tsconfigPaths: true,
   },
@@ -15,4 +16,5 @@ export default defineConfig({
   optimizeDeps: {
     include: [],
   },
+  base: '/',
 });
